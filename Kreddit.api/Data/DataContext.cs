4eb3f94f95c033836e98dc837fb5f2c3a46fb761
gustavonline <gustavbroegger@icloud.com>
2023-10-23 19:46:57 +0200

    Entity framework og SQlite generated <3

---
+++ Kreddit.api/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Kreddit.shared.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Kreddit.api.Data;
+
+public class DataContext : DbContext
+{
+    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Post> Posts => Set<Post>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    
+}

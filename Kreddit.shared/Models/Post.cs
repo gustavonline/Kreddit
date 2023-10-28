acc17258b7c9d425d10d93a35af33bd9f2357450
gustavonline <gustavbroegger@icloud.com>
2023-10-28 21:04:22 +0200

    CreatePost working. Added some styling css and html.

--- Kreddit.shared/Models/Post.cs
+++ Kreddit.shared/Models/Post.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Kreddit.shared.Models;
 
 public class Post
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters")]
     public string Title { get; set; }
     
+    [Required(ErrorMessage = "Content is required")]
+    [StringLength(500, ErrorMessage = "Content can't be longer than 500 characters")]
     public string Content { get; set; }
     
     public int UserId { get; set; }
@@ -17,7 +23,7 @@ public class Post
     
     public int Downvotes { get; set; }
     
-    public DateOnly CreatedAt { get; set; }
+    public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
     public Post(User user, string title = "", string content = "", int upvotes = 0, int downvotes = 0) {
         Title = title;
--- Kreddit.shared/Models/User.cs
+++ Kreddit.shared/Models/User.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Kreddit.shared.Models;
 
 public class User
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Username is required")]
+    [StringLength(50, ErrorMessage = "Username can't be longer than 50 characters")]
     public string Username { get; set; }
     public User(string username = "") {
         Username = username;
